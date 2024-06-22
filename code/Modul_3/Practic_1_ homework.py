b9c9d6658ec5513582b250a35fbd7c4bac3b5135
Ildana Karimova <ildanakarimova@yandex.ru>
2024-06-22 15:07:28 +0400

    Домашняя работа. Поиск однокоренных слов

---
+++ code/Modul_3/Practic_1_ homework.py
@@ -0,0 +1,16 @@
+def single_root_words(root_word, *other_words):      # * - когда мы не знаем, сколько параметров будет в переменной
+    same_words = []
+    for i in other_words:
+        if root_word in i:
+            same_words.append(i)
+            continue
+        if i.lower() in root_word.lower():
+            same_words.append(i)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
+
+
