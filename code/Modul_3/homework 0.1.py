287efbcf5c819244acdff2f6cb5ccbc77cc32d27
Ildana Karimova <ildanakarimova@yandex.ru>
2024-06-26 22:27:31 +0400

    Модуль 3. Домашняя работа

--- code/Modul_3/homework 0.1.py
+++ code/Modul_3/homework 0.1.py
@@ -1,19 +1,19 @@
 call = 0
-def count_calls(call):
+def count_calls():
+    global call
+    call += 1
     print('Количество вызовов функций ', call)
 
 def string_info(string):
-    global call
-    call += 1
     tuple_ = (len(string), string.upper(), string.lower())
+    count_calls()
     return tuple_
 
 def is_contains(string,list_):
-    global call
-    call += 1
     for item in list_:
         new_list = []
         new_list.append(item.lower())
+    count_calls()
     if string.lower() in new_list:
          return True
     else:
@@ -23,4 +23,4 @@ print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBan
 print(is_contains('cycle', ['recycle', 'cyclic'])) # No matches
-count_calls(call)
+count_calls()
