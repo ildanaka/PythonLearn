a61c049d709111230fe2bf69c1a9304f1ac609ff
Ildana Karimova <ildanakarimova@yandex.ru>
2024-08-03 15:12:39 +0400

    Модуль 6. Домашняя работа

--- code/Modul_6/Practic_1_homework.py
+++ code/Modul_6/Practic_1_homework.py
@@ -1,16 +1,60 @@
-class House:
-    def __init__(self, name, number_of_floors):
+class Plant:
+    edible = False
+    name = 'name'
+
+    def __init__(self, name, edible: bool):
         self.name = name
-        self.number_of_floors = number_of_floors
+        self.edible = edible
 
-    def go_to(self, new_floors):
-        if new_floors < 1 or new_floors > self.number_of_floors:
-            print('Такого этажа не существует')
+
+class Animal:
+    alive = True
+    fed = False
+    name = 'animal'
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food: Plant):
+        if food.edible:
+            self.fed = True
+            print(f'{self.name} съел {food.name}')
         else:
-            for i in range(1, new_floors+1):
-                print(i)
+            self.alive = False
+            print(f'{self.name} не стал есть {food.name}')
 
-h1= House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+
+class Mammal(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Predator(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name, False)
+
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name, True)
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
--- code/Modul_6/Practic_2_homework.py
+++ code/Modul_6/Practic_2_homework.py
@@ -1,28 +1,46 @@
-class House:
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+class Vehicle:
+    owner = 'владелец'
+    __model = 'model'
+    __engine_power = 1
+    __color = 'цвет'
+    __COLOR_VARIANTS = ['черный', 'белый', 'красный', 'зеленый']
 
-    def go_to(self, new_floors):
-        if new_floors < 1 or new_floors > self.number_of_floors:
-            print('Такого этажа не существует')
+    def __init__(self, owner, __model, __color, __engine_power):
+        self.owner = owner
+        self.__model = __model
+        self.__color = __color
+        self.__engine_power = __engine_power
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+
+    def print_info(self):
+        print(f'{self.get_model()},\n{self.get_horsepower()},\n{self.get_color()},\nВладелец: {self.owner}')
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
-            for i in range(1, new_floors+1):
-                print(i)
+            print(f'Нельзя сменить цвет на {new_color}')
 
-    def __len__(self):
-        return self.number_of_floors
 
-    def __str__(self):
-        return (f'Название {self.name}, количество этажей: {self.number_of_floors}')
 
-h1= House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+    def __init__(self, owner, __model, __color, __engine_power):
+        super().__init__(owner, __model, __color, __engine_power)
 
-#__str__
-print(h1)
-print(h2)
 
-#__len__
-print(len(h1))
-print(len(h2))
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1.print_info()
+vehicle1.set_color('Pink')
+vehicle1.set_color('Черный')
+vehicle1.owner = 'Vasyok'
+
+vehicle1.print_info()
+
