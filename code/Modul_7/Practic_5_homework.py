fb5f2c25561da4320e53785169fd225390eb8537
Ildana Karimova <ildanakarimova@yandex.ru>
2024-12-27 16:30:44 +0400

    Модуль 7. Домашняя работа 5

---
+++ code/Modul_7/Practic_5_homework.py
@@ -0,0 +1,17 @@
+#Файлы в операционной системе
+
+import os
+import time
+
+directory = os.getcwd()
+print('Текущая директория: ', directory)
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(directory)
+        filetime = os.path.getmtime(directory)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(directory)
+        parent_dir = os.path.dirname(directory)
+print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
+
